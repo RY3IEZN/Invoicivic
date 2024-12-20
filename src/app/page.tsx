e96fda1436c7216afcd29f505ee6022c2b58e3e6
unknown <unekue2@gmail.com>
2024-12-20 12:34:25 +0100

    added clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center max-w-5xl mx-auto gap-3 justify-center h-screen text-center">
       <h1 className="text-5xl font-bold">Invoicyn</h1>
       <p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign in</Link>
         </Button>
