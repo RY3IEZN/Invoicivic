c46a7fce5c2c7d5d545f5a986c09700fceff09e4
unknown <unekue2@gmail.com>
2025-01-02 02:16:40 +0100

    created organisation swicther

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,12 @@
 /** @format */
 
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  OrganizationSwitcher,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Container from "./Container";
 import Link from "next/link";
 
@@ -8,10 +14,16 @@ const Header = () => {
   return (
     <header>
       <Container>
-        <div className="flex justify-between gap-4 mt-5">
-          <p className="font-bold">
-            <Link href={"/dashboard"}>Invoicivic</Link>
-          </p>
+        <div className="flex justify-between gap-4 mt-5 mb-3">
+          <div className="flex items-center gap-4">
+            <p className="font-bold">
+              <Link href={"/dashboard"}>Invoicivic</Link>
+            </p>
+            <span className="text-slate-500">/</span>
+            <SignedIn>
+              <OrganizationSwitcher afterCreateOrganizationUrl={"/dashboard"} />
+            </SignedIn>
+          </div>
           <div>
             <SignedOut>
               <SignInButton />
